2b906d4e84e9e436e8ab330b05a18be9bc569df5
Michał Bogdan <michal.bogdan@gmail.com>
2016-08-02 18:39:23 +0200

    initial

--- lib/mkpb/opt.rb
+++ lib/mkpb/opt.rb
@@ -1,7 +1,35 @@
 require "mkpb/opt/version"
+require "getoptlong"
 
 module Mkpb
   module Opt
-    # Your code goes here...
+
+	class GetoptLong
+       		def to_h
+                	ret = {}
+                	self.each {|k,v| ret[k.gsub(/^-+/,"").to_sym]=v }
+                	ret
+        	end
+	end
+
+	class Opt < GetoptLong
+
+        	@h = {required: GetoptLong::REQUIRED_ARGUMENT, no: GetoptLong::NO_ARGUMENT, optional: GetoptLong::OPTIONAL_ARGUMENT, mandatory: GetoptLong::REQUIRED_ARGUMENT}
+
+        	def self.get(*ar)
+               		ret = GetoptLong.new(*ar.map{|x| x.map { |e| @h[e] ? @h[e] : e }}).to_h
+
+                	ar.inject([]) {|r,n| r << n.first.gsub(/^-+/,"").to_sym if n.last==:mandatory; r}.each do |mandatory_field|
+                        	begin   
+                                	puts "please provide --#{mandatory_field} parameter"
+                                	exit(1)
+                        	end if not ret[mandatory_field]
+
+                	end
+
+                	ret
+        	end
+	end
+
   end
 end
